ec4119cfdf490e89aab1c675bc2f7691949bf0f9
Mai Ezzat <maii3zzat@hotmail.com>
2021-01-12 20:54:31 +0200

    comments edits

--- RMIServer/src/rmi/ComplaintInterface.java
+++ RMIServer/src/rmi/ComplaintInterface.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 /**
  *
- * @author Laptop Shop
+ * @author Mai146607
  */
 public interface ComplaintInterface extends Remote {
         public void giveComplaint(Account acc ,String str) throws RemoteException;
--- RMIServer/src/rmi/RideInterface.java
+++ RMIServer/src/rmi/RideInterface.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 /**
  *
- * @author Laptop Shop
+ * @author Mai146607
  */
 public interface RideInterface extends Remote {
     public void viewRideDetails(int x) throws RemoteException;
