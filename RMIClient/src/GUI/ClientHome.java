40bd5e1afb2297ae295f270b6afbb681974a5d31
Hoda Anis <Hoda176468@bue.edu.eg>
2021-01-16 16:29:17 +0200

    Client home gui updated

--- RMIClient/src/GUI/ClientHome.java
+++ RMIClient/src/GUI/ClientHome.java
@@ -10,13 +10,10 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import rmi.ClientReadOnly;
 import rmi.ClientHomeGUI;
-import rmi.ClientInterface;
 import rmi.CurrentArea;
 
 
@@ -28,7 +25,9 @@ public class ClientHome {
         {
             this.gui = gui;
             this.r = r;
-            gui.getjButton1().addActionListener(new ClientHome.RequestBtnAction());          
+            gui.getjButton1().addActionListener(new ClientHome.RequestBtnAction());    
+            gui.getjButton2().addActionListener(new ClientHome.ViewRideBtnAction());
+            gui.getjButton3().addActionListener(new ClientHome.ComplaintBtnAction());
         }
     class RequestBtnAction implements ActionListener {
 
@@ -39,33 +38,55 @@ public class ClientHome {
                 CurrentArea d = null;
                 ClientReadOnly c = (ClientReadOnly) r.lookup("client_acc");  
                 String loc = (String)gui.getCombo().getSelectedItem();
-                if ("Maadi".equals(loc)){
-                    l = CurrentArea.MAADI; 
-                }else if ("MadintySherouk".equals(loc)){
-                   l = CurrentArea.MADINTYSHEROUK;
-                }else if("Nasrcity".equals(loc)){
-                    l = CurrentArea.NASRCITY;
-                }else if ("Zamalek".equals(loc)){
-                    l = CurrentArea.ZAMALEK;
-                }else if ("New cairo".equals(loc)){
-                    l = CurrentArea.NEWCAIRO;
-                };
+                if (null != loc)switch (loc) {
+                    case "Maadi":
+                        l = CurrentArea.MAADI;
+                        break;
+                    case "MadintySherouk":
+                        l = CurrentArea.MADINTYSHEROUK;
+                        break;
+                    case "Nasrcity":
+                        l = CurrentArea.NASRCITY;
+                        break;
+                    case "Zamalek":
+                        l = CurrentArea.ZAMALEK;
+                        break;
+                    case "New cairo":
+                        l = CurrentArea.NEWCAIRO;
+                        break;
+                    default:
+                        break;
+                }
+;
                 String dest = (String)gui.getCombo2().getSelectedItem();
-                if ("Maadi".equals(dest)){
-                    d = CurrentArea.MAADI; 
-                }else if ("MadintySherouk".equals(dest)){
-                    d= CurrentArea.MADINTYSHEROUK;
-                }else if("Nasrcity".equals(dest)){
-                    d = CurrentArea.NASRCITY;
-                }else if ("Zamalek".equals(dest)){
-                    d = CurrentArea.ZAMALEK;
-                }else if ("New cairo".equals(dest)){
-                    d = CurrentArea.NEWCAIRO;
-                };
+                if (null != dest)switch (dest) {
+                    case "Maadi":
+                        d = CurrentArea.MAADI;
+                        break;
+                    case "MadintySherouk":
+                        d= CurrentArea.MADINTYSHEROUK;
+                        break;
+                    case "Nasrcity":
+                        d = CurrentArea.NASRCITY;
+                        break;
+                    case "Zamalek":
+                        d = CurrentArea.ZAMALEK;
+                        break;
+                    case "New cairo":
+                        d = CurrentArea.NEWCAIRO;
+                        break;
+                    default:
+                        break;
+                }
                 String email = gui.getjTextField3().getText();
-
-                c.requestRide(l, d, email);
-                //add login action what happens 
+                String payment = (String)gui.getCombo3().getSelectedItem();
+                boolean p = false; 
+                if ("Yes".equals(payment))
+                    p = true;
+                else 
+                    p = false;
+                c.requestRide(l, d, email,p);
+                //in login functino go to the next gui for home client if login is correct..
                                
             } catch (Exception ex) {
                 Logger.getLogger(HomeWindow.class.getName()).log(Level.SEVERE, null, ex);
@@ -73,4 +94,34 @@ public class ClientHome {
         }
         
     }
+    class ViewRideBtnAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            try {
+                ClientReadOnly c = (ClientReadOnly) r.lookup("client_acc");  
+                String acc = gui.getjTextField4().getText(); 
+                c.viewRideHistory(); //add parameters 3nd 3omar 
+                //not fiinished
+            } catch (Exception ex) {
+                Logger.getLogger(HomeWindow.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        }
+    }
+    class ComplaintBtnAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent ae){
+             try {
+                ClientReadOnly c = (ClientReadOnly) r.lookup("client_acc");  
+                String rideID = gui.getjTextField1().getText(); 
+                String email = gui.getjTextField2().getText();
+                String complaint = gui.getjTextField5().getText(); 
+                
+                int id = Integer.parseInt(rideID); 
+                c.giveComplaint(complaint, id, email);
+                //make pop message -> done !
+            } catch (Exception ex) {
+                Logger.getLogger(HomeWindow.class.getName()).log(Level.SEVERE, null, ex);
+            }            
+        }
+}
 }
