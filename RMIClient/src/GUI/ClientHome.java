dcf9b63438427c8be01dda88bce34bbf452e6b5a
Omar Ismail <omarrashad1346@gmail.com>
2021-01-16 22:09:03 +0200

    a1

---
+++ RMIClient/src/GUI/ClientHome.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import rmi.ClientHomeGUI;
+import rmi.CurrentArea;
+import rmi.ReadOnly.ClientReadOnly;
+
+
+public class ClientHome {
+    ClientHomeGUI gui;
+    Registry r;
+    
+    public ClientHome(ClientHomeGUI gui, Registry r)
+        {
+            this.gui = gui;
+            this.r = r;
+            gui.getjButton1().addActionListener(new ClientHome.RequestBtnAction());    
+            gui.getjButton2().addActionListener(new ClientHome.ViewRideBtnAction());
+            gui.getjButton3().addActionListener(new ClientHome.ComplaintBtnAction());
+        }
+    class RequestBtnAction implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            try {
+                CurrentArea l = null; 
+                CurrentArea d = null;
+                ClientReadOnly c = (ClientReadOnly) r.lookup("client_acc");
+                String loc = (String)gui.getCombo().getSelectedItem();
+                if (null != loc)switch (loc) {
+                    case "Maadi":
+                        l = CurrentArea.MAADI;
+                        break;
+                    case "MadintySherouk":
+                        l = CurrentArea.MADINTYSHEROUK;
+                        break;
+                    case "Nasrcity":
+                        l = CurrentArea.NASRCITY;
+                        break;
+                    case "Zamalek":
+                        l = CurrentArea.ZAMALEK;
+                        break;
+                    case "New cairo":
+                        l = CurrentArea.NEWCAIRO;
+                        break;
+                    default:
+                        break;
+                }
+;
+                String dest = (String)gui.getCombo2().getSelectedItem();
+                if (null != dest)switch (dest) {
+                    case "Maadi":
+                        d = CurrentArea.MAADI;
+                        break;
+                    case "MadintySherouk":
+                        d= CurrentArea.MADINTYSHEROUK;
+                        break;
+                    case "Nasrcity":
+                        d = CurrentArea.NASRCITY;
+                        break;
+                    case "Zamalek":
+                        d = CurrentArea.ZAMALEK;
+                        break;
+                    case "New cairo":
+                        d = CurrentArea.NEWCAIRO;
+                        break;
+                    default:
+                        break;
+                }
+                String email = gui.getjTextField3().getText();
+
+                String payment = (String)gui.getCombo3().getSelectedItem();
+                boolean p = false; 
+                if ("Yes".equals(payment))
+                    p = true;
+                else 
+                    p = false;
+                c.requestRide(l, d, email,p);
+                //in login functino go to the next gui for home client if login is correct..
+                //add login action what happens 
+                               
+            } catch (Exception ex) {
+                Logger.getLogger(HomeWindow.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        }
+        
+    }
+    class ViewRideBtnAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent ae) {
+            try {
+                ClientReadOnly c = (ClientReadOnly) r.lookup("client_acc");  
+                String acc = gui.getjTextField4().getText(); 
+                //c.viewRideHistory(); //add parameters 3nd 3omar 
+                //not fiinished
+            } catch (Exception ex) {
+                Logger.getLogger(HomeWindow.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+        }
+    }
+
+    class ComplaintBtnAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent ae){
+             try {
+                ClientReadOnly c = (ClientReadOnly) r.lookup("client_acc");  
+                String rideID = gui.getjTextField1().getText(); 
+                String email = gui.getjTextField2().getText();
+                String complaint = gui.getjTextField5().getText(); 
+                
+                int id = Integer.parseInt(rideID); 
+                c.giveComplaint(complaint, id, email);
+                //make pop message -> done !
+            } catch (Exception ex) {
+                Logger.getLogger(HomeWindow.class.getName()).log(Level.SEVERE, null, ex);
+            }            
+        }
+}
+}
--- RMIClient/src/rmi/RMIClient.java
+++ RMIClient/src/rmi/RMIClient.java
@@ -3,7 +3,48 @@ package rmi;
 public class RMIClient {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         
+=======
+
+        try{
+            Registry registry = LocateRegistry.getRegistry(1080);
+            ClientReadOnly client_acc = (ClientReadOnly) registry.lookup("Client Account");
+            ClientInterface client = (ClientInterface) registry.lookup("Client");
+            ClientReadOnly client_ride = (ClientReadOnly) registry.lookup("Client Ride");
+/*
+
+            String sDate1="12/2025";
+             Date date1 = new SimpleDateFormat("MM/yyyy").parse(sDate1);
+
+            client_acc.createClientAccount("Omar", "1346", "ahmed134646", "124679", AccType.CLIENT,
+                    2500,"1234 2225 6987 1025",882,date1);
+*/
+
+            //client_acc.login("ahmed134646","1346");
+            Account acc = new Account();
+            acc.login()
+            client_ride.requestRide(CurrentArea.MAADI, CurrentArea.MADINTYSHEROUK,"ahmed134646",true);
+            client_ride.requestRide(CurrentArea.MAADI, CurrentArea.NEWCAIRO,"ahmed134646",false);
+            //System.out.println(acc.viewAllAccounts());
+            //client.editAccount();
+            //System.out.println(client_acc.viewOwnAccount());
+            //client_ride.requestRide(CurrentArea.MAADI, CurrentArea.MADINTYSHEROUK);
+            //client.editAccount();
+            //System.out.println(client_acc.viewOwnAccount());
+            //client_acc.login("omar1346","1346");
+            //client.editAccount();
+            //client_acc.viewOwnAccount();
+//try
+        }catch (Exception ex){
+            ex.printStackTrace();
+            System.out.println(ex);
+        }
+        /*acc.createClientAccount("Omar", "1346", "omar1346", "124679", AccType.CLIENT,
+                    2500,"1234 2225 6987 1025",882,date1);
+           acc.createClientAccount("Ahmed", "1346", "ahmed1346", "124679", AccType.CLIENT,
+                    2500,"1234 5555 5555 1025",882,date2);*/
+>>>>>>> Stashed changes
     }
     
 }
